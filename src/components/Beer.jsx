892f97171bc108db01b0f56e672382120b97ac0a
toth_szilvia <szylwy@gmail.com>
2022-04-08 15:58:24 +0200

    html with all data and object destructuring

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -1,10 +1,21 @@
-function Beer (props) {
+// import dear from '../image/ozike.jpg';
+
+// function importAll(requiredContext) {
+//     let images = {};
+//     requiredContext.keys().map((item) => { images[item.replace('./', '')] = requiredContext(item); });
+//     return images;
+// }
+
+// const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
+function Beer ({title, sub, text, img} ) {
      return (
         <div className="beer">
-            <div className="beer">
-                {props.beerInfo.title}
-                {/* {props.randomNumber} */}
-            </div>
+              {/* <img src={images['doggy.png']} /> */}
+              <img src={require('../image/ozike.jpg')} />
+            <h2>{title}</h2>
+            <p className="sub">{sub}</p>
+            <p className="text">{text}</p>
         </div>
      );
 }
--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -4,7 +4,7 @@ function Beers(props) { //props, properties, mert ez nem parameter --> a beersLi
     console.log(props.beersList);
     return (
         <div className="beers">
-            {props.beersList.map(beer => <Beer beerInfo={beer} />)}
+            {props.beersList.map(beer => <Beer title={beer.title} sub={beer.sub} text={beer.text} img={beer.img} />)}
             {/* randomNumber={10} */}
             {/* {title, sub, text, img} */}
         </div>
